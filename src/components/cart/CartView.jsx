9f8fdc6202f39186317265bbc9ece2570a6aa4c1
scottyadean <scottyadean@gmail.com>
2024-02-29 16:10:45 -0700

    added table view for cart

--- src/components/cart/CartView.jsx
+++ src/components/cart/CartView.jsx
@@ -1,33 +1,36 @@
 //import React from 'react'
 import { removeFromCart } from '../../state/reducers/itemReducer';
 import { useSelector, useDispatch } from "react-redux";
+import { Divider} from 'antd';
+import CartItem from './CartItem';
 
 export default function CartView() {
 
     const dispatch = useDispatch();
 
     const cart = useSelector((state)=>state.items.cart)
-    const renderCartItems = (collection) => {
-        const html = [];
+    const formatCartItems = (collection) => {
+        const output = [];
         for( let key in collection){
             let data = collection[key]; 
-            html.push( { _id: key, count: data.count, name: data.item.name, price: data.price, cost_per_unit: data.item.price } );
+            output.push( { _id: key, count: data.count, name: data.item.name, price: data.price, cost_per_unit: data.item.price } );
         }
-
-        
-        return html; 
+        return output; 
     }
 
   return (
     <div>
-        cart view
-        { renderCartItems(cart).map( (c)=>(
-        <div key={`cart-item-${c._id}`}> 
-           
-            {c.name}  | {c.count} | {c.price} |
-             <button onClick={()=>dispatch(removeFromCart( { item: { _id: c._id, price: c.cost_per_unit } } ))}> remove </button>
-        
-        </div>) )  }
-    </div>
-  )
+    <h3 className='pos-h3'>Cart</h3>
+    <Divider style={{borderBottom:"1px solid #333",padding:0, margin: 5}} />
+
+    <table style={{width:"100%"}}>
+        <tbody>
+        { formatCartItems(cart).map( (item, idx)=>( <CartItem 
+                                                      item={item} 
+                                                      key={idx} 
+                                                      callback={ removeFromCart } 
+                                                      dispatch={dispatch} /> ) )  }
+        </tbody>
+    </table>
+    </div>)
 }
